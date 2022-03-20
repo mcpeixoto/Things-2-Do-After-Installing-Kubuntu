26565dea56cd9e10b251a3d37ca1fc91108fa86b
Miguel Peixoto <miguelpeixoto457@gmail.com>
2022-03-20 00:22:33 +0000

    small improvements

--- run.sh
+++ run.sh
@@ -11,6 +11,7 @@ cd ~/Downloads
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg -i google-chrome-stable_current_amd64.deb
 
+# Spotify
 curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | apt-key add - 
 echo "deb http://repository.spotify.com stable non-free" | tee /etc/apt/sources.list.d/spotify.list
 apt-get install spotify-client
