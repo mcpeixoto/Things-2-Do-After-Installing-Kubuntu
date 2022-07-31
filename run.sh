c22c1ce63182a5f2d9dce876e9c971d48d6f406d
Miguel Caçador Peixoto <miguelpeixoto457@gmail.com>
2022-07-31 10:00:31 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,21 +3,27 @@ apt-get update -y && apt-get upgrade -y && apt-get dist-upgrade -y
 
 
 # Install packages
-apt-get install yakuake python3-pip libclang-dev libudev-dev snap git rustc cargo make libusb-1.0-0-dev libdbus-1-dev llvm libclang-dev gnome-keyring gnome-disk-utility python-is-python3 htop python3-tk python3-pil python3-pil.imagetk nvtop cmake protobuf-compiler curl python-dev kate dolphin aptitude -y
+apt-get install wget yakuake python3-pip libclang-dev libudev-dev snap git rustc cargo make libusb-1.0-0-dev libdbus-1-dev llvm libclang-dev gnome-keyring gnome-disk-utility python-is-python3 htop python3-tk python3-pil python3-pil.imagetk nvtop cmake protobuf-compiler curl kate dolphin aptitude -y
 
-snap install snap-store skype fluxgui telegram-desktop discord skype
+snap install fluxgui telegram-desktop skype
 
 pip3 install torch pytorch-lightning numpy pandas tables keras tensorflow jupyter sklearn torchvision matplotlib ipython-autotime
 
+# Discord
+cd /tmp
+wget -O ~/discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
+dpkg -i discord.deb
+
+
 # INSTALL CHROME
-cd ~/Downloads
+cd /tmp
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg -i google-chrome-stable_current_amd64.deb
 
 # Spotify
 curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | apt-key add - 
 echo "deb http://repository.spotify.com stable non-free" | tee /etc/apt/sources.list.d/spotify.list
-apt-get install spotify-client
+apt-get update && apt-get install spotify-client
 
 # Stop skype from mutting apps
 sed -i 's/load-module module-role-cork/#load-module module-role-cork/' /etc/pulse/default.pa
