f38b10e246c0e829780d79f088ec5d762339f20b
Miguel Caçador Peixoto <miguelpeixoto457@gmail.com>
2023-01-20 14:30:24 +0000

    added brave

--- run.sh
+++ run.sh
@@ -99,3 +99,18 @@ sudo apt autoremove -y
 # Alias
 echo "alias ll='ls -l'" >> ~/.bashrc
 echo "alias cenv='python -m venv .env'" >> ~/.bashrc
+echo "alias berry='ssh  mcpeixoto@193.137.11.249 -p 51000'" >> ~/.bashrc
+
+
+
+# Install brave
+sudo apt install curl -y
+
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+sudo apt update
+
+sudo apt install brave-browser -y
+
